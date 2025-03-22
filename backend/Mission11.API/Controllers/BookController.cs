422cd624b0048e9868a1d819c084892ceae63772
tylerdemann <tylerdemann@gmail.com>
2025-03-21 22:41:12 -0700

    added pageination

--- backend/Mission11.API/Controllers/BookController.cs
+++ backend/Mission11.API/Controllers/BookController.cs
@@ -11,9 +11,20 @@ namespace Mission11.API.Controllers
         private BookContext _bookContext;
         public BookController(BookContext temp) => _bookContext = temp;
 
-        public IEnumerable<Book> Get()
+        public IActionResult Get(int pageHowMany = 5, int pageNum = 1)
         {
-            return _bookContext.Books.ToList();
+            var something = _bookContext.Books
+                .Skip((pageNum - 1) * pageHowMany)
+                .Take(pageHowMany)
+                .ToList();
+            
+            var totalNumBooks = _bookContext.Books.Count();
+            
+            return Ok(new
+            {
+                Books = something,
+                TotalNumBooks = totalNumBooks
+            });
         }
     }
 }
--- frontend/src/BookList.tsx
+++ frontend/src/BookList.tsx
@@ -4,25 +4,31 @@ import { Book } from "./types/Book";
 function BookList() {
 
     const [Books, setBooks] = useState<Book[]>([]);
+    const [pageSize, setPageSize] = useState<number>(5);
+    const [pageNum, setPageNum] = useState<number>(1);
+    const [totalItems, setTotalItems] = useState<number>(0);
+    const [totalPages, setTotalPages] = useState<number>(0);
 
     useEffect(() => {
         const fetchBooks = async () => {
-            const response = await fetch("https://localhost:5000/api/Book");
+            const response = await fetch(`https://localhost:5000/api/Book?pageHowMany=${pageSize}&pageNum=${pageNum}`);
             const data = await response.json();
-            setBooks(data);
+            setBooks(data.books);
+            setTotalItems(data.totalNumBooks)
+            setTotalPages(Math.ceil(totalItems / pageSize))
         };
         fetchBooks();
-    }, []);
+    }, [pageSize, pageNum, totalItems]);
 
     return (
         <>
             <h1>Book Store</h1>
             <br/>
             {Books.map((b) => (
-                <div id="bookCard">
-                    <h3>{b.title}</h3>
-
-                    <ul>
+                <div id="bookCard" className='card' key={b.bookID}>
+                    <h3 className='card-title'>{b.title}</h3>
+                    <div className='card-body'>
+                    <ul className='list-unstyled'>
                         <li>Author: {b.author}</li>
                         <li>Publisher: {b.publisher}</li>
                         <li>ISBN: {b.isbn}</li>
@@ -31,8 +37,35 @@ function BookList() {
                         <li>Page Count: {b.pageCount}</li>
                         <li>Price: {b.price}</li>
                     </ul>
+                    </div>
+
+                    
                 </div>
             ))}
+
+            <button disabled={pageNum === 1} onClick={() => setPageNum(pageNum - 1)}>Previous</button>
+
+            {[...Array(totalPages)].map((_, i) => (
+                <button key={i+1} onClick={() => setPageNum(i+1)} disabled={pageNum === (i+1)}>
+                    {i+1}
+                </button>
+            ))}
+
+            <button disabled={pageNum === totalPages}onClick={() => setPageNum(pageNum + 1)}>Next</button>
+
+            <br />
+            <label>
+                Results per page:
+                <select value={pageSize} onChange={(b) => {
+                setPageSize(Number(b.target.value));
+                    setPageNum(1);
+                }}
+                >
+                    <option value='5'>5</option>
+                    <option value='10'>10</option>
+                    <option value='20'>20</option>
+                </select>
+            </label>
         </>
     );
 }
