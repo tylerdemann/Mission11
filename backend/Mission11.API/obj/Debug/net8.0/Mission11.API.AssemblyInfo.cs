f343f4063381b2b43c636e26c250ff1b58673d8c
tylerdemann <tylerdemann@gmail.com>
2025-04-04 19:16:43 -0600

    Final updates before submitting

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f559186412334595739068b9e577e7502f49aaa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0239b2d08b5cb4d7dee60b550fd61d75a35415b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
